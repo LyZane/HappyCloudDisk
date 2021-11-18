fbe7f9fc588818fd88afd44f2d6e8ada4e4b296f
zane.luo <zane.luo@welab-inc.com>
2021-11-18 23:13:48 +0800

    init project by electron-vue

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'happy-cloud-disk'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
